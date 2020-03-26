cb22ee52ca605b688cc50b2eef363f18321b2c27
me <me@me.me.com>
2020-03-26 13:56:55 -0700

    add vervet monkey section

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,44 @@
 $(document).ready(function(){
 
-  $(".sidebar").click(
+  $("#showthesidebar").click(
     function(){
-      $(".sidebar").hide();
+      $(".sidebar").show();
+      $("#hidethesidebar").show();
+      $("#showthesidebar").hide();
     });
 
+  $("#hidethesidebar").click(
+    function(){
+      $(".sidebar").hide();
+      $("#hidethesidebar").hide();
+      $("#showthesidebar").show();
+    });
+
+  $("#warthogs").click(function(){
+    $(".warthogs").show();
+    $("#warthogs").hide();
+    $("#stopwarthogs").show();
+
+  })
+
+  $("#stopwarthogs").click(function(){
+    $(".warthogs").hide();
+    $("#warthogs").show();
+    $("#stopwarthogs").hide();
+  })
+
+  $("#vervetmonkeys").click(function(){
+    $(".vervetmonkeys").show();
+    $("#vervetmonkeys").hide();
+    $("#stopvervetmonkeys").show();
+
+  })
+
+  $("#stopvervetmonkeys").click(function(){
+    $(".vervetmonkeys").hide();
+    $("#vervetmonkeys").show();
+    $("#stopvervetmonkeys").hide();
+  })
 
 
 
