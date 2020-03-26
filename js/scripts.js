5ac06af00c70e9859ad6c080e3dda71029f0fcfa
me <me@me.me.com>
2020-03-26 15:09:15 -0700

    refactor the code to use toggle instead

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,25 @@
 $(document).ready(function(){
 
-  $("#showthesidebar").click(
-    function(){
-      $(".sidebar").show();
-      $("#hidethesidebar").show();
-      $("#showthesidebar").hide();
-    });
+ 
 
-  $("#hidethesidebar").click(
-    function(){
-      $(".sidebar").hide();
-      $("#hidethesidebar").hide();
-      $("#showthesidebar").show();
-    });
-
-  $("#warthogs").click(function(){
-    $(".warthogs").show();
-    $("#warthogs").hide();
-    $("#stopwarthogs").show();
-
-  })
-
-  $("#stopwarthogs").click(function(){
-    $(".warthogs").hide();
-    $("#warthogs").show();
-    $("#stopwarthogs").hide();
-  })
-
-  $("#vervetmonkeys").click(function(){
-    $(".vervetmonkeys").show();
-    $("#vervetmonkeys").hide();
-    $("#stopvervetmonkeys").show();
-
-  })
-
-  $("#stopvervetmonkeys").click(function(){
-    $(".vervetmonkeys").hide();
-    $("#vervetmonkeys").show();
-    $("#stopvervetmonkeys").hide();
-  })
-
-  $("#honeybadgers").click(function(){
-    $(".honeybadgers").show();
-    $("#honeybadgers").hide();
-    $("#stophoneybadgers").show();
-
-  })
-
-  $("#stophoneybadgers").click(function(){
-    $(".honeybadgers").hide();
-    $("#honeybadgers").show();
-    $("#stophoneybadgers").hide();
-  })
+  $(".warthogstoggle").click(function(){
+    $(".warthogs").toggle();
+    $(".warthogstoggle").toggle();
+  });
 
 
+  $(".vervetmonkeystoggle").click(function(){
+    $(".vervetmonkeys").toggle();
+    $(".vervetmonkeystoggle").toggle();
+  });
 
 
+  $(".honeybadgerstoggle").click(function(){
+    $(".honeybadgers").toggle();
+    $(".honeybadgerstoggle").toggle();
+   
+  });
 
-
+ 
 
 });
